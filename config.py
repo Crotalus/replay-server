1a0f8121420bc86976521c520df3e11b0cba3eaf
Crotalus <Crotalus@users.noreply.github.com>
2016-07-15 01:33:27 +0200

    zip fix

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ REPLAY_FOLDER = "vault/replay_vault/"
 STREAMING_FOLDER = REPLAY_FOLDER + "streaming/"
 # Contains processed replays which didn't get into database for some reason
 PENDING_FOLDER = REPLAY_FOLDER + "pending/"
-
+# asynchronous zipping of replay
 ASYNC_ZIP = True
 # interval between each disk flush in seconds
 FLUSH_INTERVAL = 30
