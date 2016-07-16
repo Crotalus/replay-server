49fd1a701aca393b35a95f05805fa6582a0eb77d
Crotalus <Crotalus@users.noreply.github.com>
2016-07-16 18:16:17 +0200

    Let ReplayFile handle gritty details of replays

--- replay_server/peer.py
+++ replay_server/peer.py
@@ -1,16 +1,11 @@
-import sys
 import os
-import asyncio
-import zlib
-import zipfile
-import base64
-import json
 import logging
-
-import aiomysql
-
+import asyncio
+import json
 import config
-import db
+
+import ReplayFile
+
 
 log = logging.getLogger(__name__)
 
@@ -20,7 +15,6 @@ class ReplayPeer:
     def __init__(self, client):
         self.client = client
         self.address = self.client.address
-
         self.sent_step = -1  # Last step sent, -1 due to header
 
     def __str__(self):
@@ -35,9 +29,10 @@ class ReplayPeer:
 
 
 class ReplayFilePeer:
+
     def __init__(self, stream):
         self.stream = stream
-        self.file = open(self.streaming_path, 'wb')
+        self.file = open(self.streaming_replay_path, 'wb')
         self.sent_step = -1  # Last step sent, -1 due to header
 
     def __str__(self):
@@ -48,44 +43,19 @@ class ReplayFilePeer:
         return self.stream.game_id
 
     @property
-    def nestedpath(self):
-        # copypasted this mess from old replay server
-        path = config.REPLAY_FOLDER
-        dirsize = 100
-        depth = 5
-        i = depth
-        dirname = path
-        while i > 1:
-            dirname = os.path.join(dirname, str(
-                (self.game_id // (dirsize ** (i - 1))) % dirsize))
-            i = i - 1
-
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-
-        return dirname
-
-    @property
-    def streaming_path(self):
+    def streaming_replay_path(self):
         return os.path.join(config.STREAMING_FOLDER, '%d.scfareplay' % self.game_id)
 
     @property
     def info_path(self):
         return os.path.join(config.STREAMING_FOLDER, '%d.json' % self.game_id)
 
-    @property
-    def pending_path(self):
-        return os.path.join(config.PENDING_FOLDER, '%d.fafreplay' % self.game_id)
-
-    @property
-    def final_path(self):
-        return os.path.join(self.nestedpath, '%d.fafreplay' % self.game_id)
-
     def send(self, step_data):
         self.file.write(step_data)
         self.sent_step += 1
         if config.FLUSH_INTERVAL > 0 and not self.sent_step % (config.FLUSH_INTERVAL * 10):
             self.file.flush()
+            self.save_infofile(self.get_streaminfo())
 
     def finish(self):
         self.file.close()
@@ -93,95 +63,12 @@ class ReplayFilePeer:
         asyncio.ensure_future(self.persist_replay(), loop=loop)
 
     def get_streaminfo(self):
-        return dict(desynced=self.stream.desynced, featured_mod='faf', ticks=self.stream.step, uid=self.game_id)
+        return dict(complete=self.stream.game_ended, desynced=self.stream.desynced, featured_mod='faf', ticks=self.stream.step, , uid=self.game_id)
 
     def save_infofile(self, info):
-        with open(self.info_path, 'w') as file:
+        with open(self.info_file_path, 'w') as file:
             json.dump(info, file)
 
-    async def get_gameinfo(self):
-        info = None
-
-        pool = await db.get_pool()
-        async with pool.get() as conn:
-            cursor = await conn.cursor(aiomysql.DictCursor)
-
-            query = """
-                SELECT f.gamemod, gs.gameType, m.filename, gs.gameName, gps.playerId, gps.AI, gps.team
-                FROM game_stats gs LEFT JOIN game_player_stats gps ON gps.gameId = gs.id LEFT JOIN table_map m ON m.id = gs.mapId
-                LEFT JOIN game_featuredMods f ON gs.gameMod = f.id WHERE gs.id = '%s'
-            """
-
-            # Do we really need all these LEFT JOINs? Seems like we should try
-            # to keep table relations more sane
-            """
-                SELECT f.gamemod, gs.gameType, m.filename, gs.gameName, l.host, l.login, gps.playerId, gps.AI, gps.team
-                FROM game_stats gs LEFT JOIN game_player_stats gps ON gps.gameId = gs.id LEFT JOIN table_map m ON m.id = gs.mapId
-                LEFT JOIN logins l ON l.id = gps.playerid LEFT JOIN game_featuredMods f ON gs.gameMod = f.id WHERE gs.id = '%s'
-            """
-
-            await cursor.execute(query, self.game_id)
-            row = await cursor.fetchone()
-            if row is not None:
-                info = row
-
-        return info
-
     async def persist_replay(self):
-        info = self.get_streaminfo()
-        # if something goes wrong, still keep a file with the json data
-        self.save_infofile(info)
-
-        ext_info = await self.get_gameinfo()
-        if ext_info:
-            info.update(ext_info)
-
-        self.save_infofile(info)
-
-        """
-        if config.ASYNC_ZIP:
-            await self.create_zipreplay_thread()
-        else:
-            self.create_zipreplay()
-        """
-        self.create_legacy_fafreplay()
-
-        # we survived the fafreplay creation part, delete the temporary files
-        os.remove(self.streaming_path)
-        os.remove(self.info_path)
-
-        await self.insert_replay()
-
-        # in the future, we should use db transaction to make sure we can rollback if this rename fails for some reason
-        os.rename(self.pending_path, self.final_path)
-
-    def create_legacy_fafreplay(self):
-        with open(self.info_path, 'r') as i_file, open(self.streaming_path, 'rb') as s_file, open(self.pending_path, 'wb') as p_file:
-            infodata = i_file.read()
-            replaydata = s_file.read()
-            p_file.write((infodata + "\n").encode('utf-8'))
-            p_file.write(base64.b64encode(zlib.compress(replaydata)))
-
-    def create_fafreplay(self):
-        with open(self.info_path, 'r') as i_file, open(self.streaming_path, 'rb') as s_file, open(self.pending_path, 'wb') as p_file:
-            infodata = i_file.read()
-            replaydata = s_file.read()
-            p_file.write((infodata + "\n").encode('utf-8'))
-            p_file.write(zlib.compress(replaydata))
-
-    def create_zipreplay(self):
-        with zipfile.ZipFile(self.pending_path, 'w') as z_file:
-            z_file.write(self.info_path, os.path.basename(self.info_path))
-            z_file.write(self.streaming_path, os.path.basename(self.streaming_path))
-
-    async def create_zipreplay_thread(self):
-        loop = asyncio.get_event_loop()
-        await loop.run_in_executor(None, self.create_zipreplay)
-
-    async def insert_replay(self):
-        pool = await db.get_pool()
-        async with pool.get() as conn:
-            cursor = await conn.cursor()
-            await cursor.execute("INSERT INTO game_replays (uid, ticks, desynced) VALUES ('%s', '%s', '%s')", (self.game_id, self.stream.step, self.stream.desynced))
-
-
+        replay = ReplayFile(self.streaming_replay_path, self.get_streaminfo())
+        await replay.persist()
---
+++ replay_server/replayfile.py
@@ -0,0 +1,160 @@
+import os
+import asyncio
+import json
+import base64
+import zlib
+import zipfile
+import logging
+import aiomysql
+
+import db
+
+import config
+
+
+class ReplayFile:
+
+    def __init__(self, file, info=None):
+        self.id = os.path.basename(file)
+        self.info = info
+
+    def load_info(self, file):
+        with open(file, 'r') as fd:
+            self.info = json.load(fd)
+
+    def save_info(self, file):
+        with open(file, 'w') as fd:
+            json.dump(self.info, fd)
+
+    @property
+    def nestedpath(self):
+        # copypasted this mess from old replay server
+        path = config.REPLAY_FOLDER
+        dirsize = 100
+        depth = 5
+        i = depth
+        dirname = path
+        while i > 1:
+            dirname = os.path.join(dirname, str(
+                (self.id // (dirsize ** (i - 1))) % dirsize))
+            i = i - 1
+
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+        return dirname
+
+    def pending_file(self, ext='fafreplay'):
+        return os.path.join(config.PENDING_FOLDER, '%d.%s' % self.id, ext)
+
+    def streaming_file(self, ext='screplay'):
+        return os.path.join(config.STREAMING_FOLDER, '%d.%s' % self.id, ext)
+
+    def final_file(self, ext='fafreplay'):
+        return os.path.join(self.nestedpath, '%d.%s' % self.id, ext)
+
+    async def get_gameinfo(self):
+        info = None
+
+        pool = await db.get_pool()
+        async with pool.get() as conn:
+            cursor = await conn.cursor(aiomysql.DictCursor)
+
+            query = """
+                SELECT f.gamemod, gs.gameType, m.filename, gs.gameName, gps.playerId, gps.AI, gps.team
+                FROM game_stats gs LEFT JOIN game_player_stats gps ON gps.gameId = gs.id LEFT JOIN table_map m ON m.id = gs.mapId
+                LEFT JOIN game_featuredMods f ON gs.gameMod = f.id WHERE gs.id = '%s'
+            """
+
+            # Do we really need all these LEFT JOINs? Seems like we should try
+            # to keep table relations more sane
+            """
+                SELECT f.gamemod, gs.gameType, m.filename, gs.gameName, l.host, l.login, gps.playerId, gps.AI, gps.team
+                FROM game_stats gs LEFT JOIN game_player_stats gps ON gps.gameId = gs.id LEFT JOIN table_map m ON m.id = gs.mapId
+                LEFT JOIN logins l ON l.id = gps.playerid LEFT JOIN game_featuredMods f ON gs.gameMod = f.id WHERE gs.id = '%s'
+            """
+
+            await cursor.execute(query, self.info.uid)
+            row = await cursor.fetchone()
+            if row is not None:
+                info = row
+
+        return info
+
+    async def insert_replay(self):
+        pool = await db.get_pool()
+        async with pool.get() as conn:
+            cursor = await conn.cursor()
+            await cursor.execute("INSERT INTO game_replays (uid, ticks, desynced) VALUES ('%s', '%s', '%s')", (self.info.uid, self.info.ticks, self.info.desynced))
+
+    async def persist(self):
+        if os.path.isfile(self.final_file()):
+            return
+
+        # Move replay /info from streaming/ -> pending/ folder, XXX: check if not running
+        s_file = self.streaming_file('screplay')
+        i_file = self.streaming_file('json')
+        if os.path.isfile(s_file):
+            if not self.info:
+                self.load_info(i_file)
+            else:
+                self.save_info(i_file)
+
+            os.rename(s_file, self.pending_file('screplay'))
+            os.rename(i_file, self.pending_file('json'))
+
+        # Create fafreplay in pending/
+        fafreplay = self.pending_file('fafreplay')
+        if not os.path.isfile(fafreplay):
+            i_file = self.pending_file('json')
+            if not self.info:
+                self.load_info(i_file)
+
+            if True:  # XXX: ext_info check here
+                ext_info = await self.get_gameinfo()
+                if ext_info:
+                    self.info.update(ext_info)
+                    self.save_info(i_file)
+
+            """
+            if config.ASYNC_ZIP:
+                await self.create_zipreplay_thread()
+            else:
+                self.create_zipreplay()
+            """
+            self.create_fafreplay()
+            os.remove(i_file)
+            os.remove(self.pending_file('screplay'))
+
+        await self.insert_replay()
+
+        # in the future, we should use db transaction to make sure we can
+        # rollback if this rename fails for some reason
+        os.rename(fafreplay, self.final_file())
+
+    def create_fafreplay(self, legacy=True):
+        info_file = self.pending_file('json')
+        screplay = self.pending_file('screplay')
+        fafreplay = self.pending_file('fafreplay')
+
+        with open(info_file, 'r') as i_file, open(screplay, 'rb') as sc, open(fafreplay, 'wb') as faf:
+            info = i_file.read()
+            faf.write((info + "\n").encode('utf-8'))
+            if legacy:
+                #  legacy format uses base64 encoding
+                faf.write(base64.b64encode(zlib.compress(sc.read())))
+            else:
+                faf.write(zlib.compress(sc.read()))
+
+    def create_zipreplay(self):
+        with zipfile.ZipFile(self.pending_fafreplay_path, 'w') as z_file:
+            z_file.write(self.info_file_path,
+                         os.path.basename(self.info_file_path))
+            z_file.write(self.streaming_replay_path,
+                         os.path.basename(self.streaming_replay_path))
+
+    async def create_zipreplay_thread(self):
+        loop = asyncio.get_event_loop()
+        await loop.run_in_executor(None, self.create_zipreplay)
+
+
